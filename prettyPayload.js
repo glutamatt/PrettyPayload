3e590c509b4073ce0a2e222e30fbb5d21c7506bd
Mathieu Louafi <mathieu.louafi@blablacar.com>
2014-08-06 17:07:13 +0200

    initial version

---
+++ prettyPayload.js
@@ -0,0 +1,11 @@
+function () {
+    var elements = document.getElementsByClassName('msg-payload');
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        try {
+            element.innerHTML = '<pre>' + JSON.stringify(JSON.parse(element.innerHTML), null, 4) + '</pre>';
+        } catch (e) {
+            // not a JSON string
+        }
+    }
+}();
