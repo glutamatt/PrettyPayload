04c40617ea3af251b3e6e2c09d07d6b59cce49f9
glutamatt <fatal974@hotmail.com>
2014-08-11 15:29:43 +0200

    Use existing jQuery from Rabbit Web UI

--- prettyPayload.js
+++ prettyPayload.js
@@ -1,11 +1,3 @@
-(function () {
-    var elements = document.getElementsByClassName('msg-payload');
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        try {
-            element.innerHTML = '<pre>' + JSON.stringify(JSON.parse(element.innerHTML), null, 4) + '</pre>';
-        } catch (e) {
-            // not a JSON string
-        }
-    }
-})();
+$('pre.msg-payload').each(function(i,e){
+    var p = $(e); p.html(JSON.stringify(JSON.parse(p.html()),null,4))
+})
